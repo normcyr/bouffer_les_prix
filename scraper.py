23549bd332f3a34b94865ad44253a76b6d3d9da8
Normand Cyr <normand.cyr@gmail.com>
2018-04-04 09:40:59 -0400

    extraire résultats et sortie json des résultats

---
+++ scraper.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Aller chercher les prix via http://www.supermarches.ca/epicerie.asp
+'''
+
+from bs4 import BeautifulSoup
+import requests
+import json
+from datetime import datetime
+
+def faire_soupe(url_recherche):
+
+    '''
+    requete = requests.get(url_recherche)
+
+    if requete.status_code == requests.codes.ok:
+        # crée la soupe
+        soupe = BeautifulSoup(requete.text, 'html.parser')
+    else:
+        # pleure parce que la page n'est pas disponible
+        requete.raise_for_status()
+    '''
+
+    # pour effectuer les tests hors-ligne
+    with open('exemple2.html', 'r') as fichier_html:
+        soupe = BeautifulSoup(fichier_html, 'html.parser')
+
+    return(soupe)
+
+def trouver_resultats(soupe):
+
+    # trouver section nombre de résultats et extraire le chiffre
+    section_nb_produits = soupe.find('table', {'id': 'table89'})
+    nb_produits = int(section_nb_produits.find('b').text)
+    #print('nb produits: {}'.format(nb_produits))
+
+    # trouver section liste des résultats
+    soupe_resultats = soupe.find_all('tr', {'onmouseover': 'this.bgColor = \'#FFFFD9\''})
+
+    return(soupe_resultats)
+
+def creer_liste(soupe_resultats):
+
+    # initialiser liste de résultats
+    liste_speciaux = []
+
+    # extraire l'information
+    for resultat in soupe_resultats:
+        nom_produit = resultat.find('td', {'width': '297'}).text.strip().replace('\n', ' ')
+        origine_produit = resultat.find('td', {'width': '93'}).text.strip()
+        prix_produit = float(resultat.find('td', {'width': '78'}).text.strip().replace(' / ch.', ''))
+        periode_special_produit = resultat.find('td', {'width': '74'}).text.strip()
+        magasin_produit = resultat.find('td', {'width': '72'}).text.strip()
+
+        # enlever la mention de l'unité de mesure et mettre format sur même unité (ici: gramme)
+        format_brut_produit = resultat.find('td', {'width': '86'}).text.strip()
+        if format_brut_produit[-2:] == 'kg':
+            format_produit = int(format_brut_produit[:-2]) * 1000
+        else:
+            format_produit = int(format_brut_produit[:-2])
+
+        # calculer coût par 100 g
+        cout_100g_produit = prix_produit / (format_produit / 100)
+
+        # créer dictionnaire des résultats
+        details_produit = {'nom': nom_produit, 'format': format_produit, 'origine': origine_produit, 'prix': prix_produit, 'prix par 100 g': cout_100g_produit, 'période spécial': periode_special_produit, 'magasin': magasin_produit}
+
+        # ajouter le dictionnaire à la liste de résultats
+        liste_speciaux.append(details_produit)
+
+    return(liste_speciaux)
+
+def extraire_json(liste_speciaux, fichier_sortie):
+
+    with open(fichier_sortie, 'w') as outfile:
+        json.dump(liste_speciaux, outfile, indent = 4)
+
+def main():
+
+    recherche_url_base = 'http://www.supermarches.ca/pages/default.asp?t=&tr=&vd=&ig=&q=rech&cid=&query='
+    mots_recherche = 'Beurre+d\'arachide+Croquant+Kraft'
+    url_recherche = recherche_url_base + mots_recherche
+    ajd = datetime.now().strftime("%Y%m%d")
+    fichier_sortie = 'liste_speciaux_' + ajd + '.json'
+
+    soupe = faire_soupe(url_recherche)
+    soupe_resultats = trouver_resultats(soupe)
+    liste_speciaux = creer_liste(soupe_resultats)
+
+    extraire_json(liste_speciaux, fichier_sortie)
+
+if __name__ == '__main__':
+    main()
