0d9fdb7efc8a3b28c3af04035dd5cd0f9f971ffd
Normand Cyr <normand.cyr@gmail.com>
2018-05-29 23:56:16 -0400

    préparer pour utiliser une liste d'items à chercher

--- scraper.py
+++ scraper.py
@@ -51,8 +51,7 @@ def trouver_resultats(soupe):
 
     return(soupe_resultats)
 
-
-def creer_liste(soupe_resultats):
+def creer_liste(soupe_resultats, mot):
 
     # initialiser liste de résultats
     liste_speciaux = []
@@ -61,31 +60,22 @@ def creer_liste(soupe_resultats):
     for resultat in soupe_resultats:
         nom_produit = resultat.find('td', {'width': '297'}).text.strip().replace('\n', ' ')
         origine_produit = resultat.find('td', {'width': '93'}).text.strip()
-        prix_produit = float(resultat.find('td', {'width': '78'}).text.strip().replace(' / ch.', ''))
+        prix_produit = resultat.find('td', {'width': '78'}).text.strip().replace(' / ch.', '')
         periode_special_produit = resultat.find('td', {'width': '74'}).text.strip()
         magasin_produit = resultat.find('td', {'width': '72'}).text.strip()
-
-        # enlever la mention de l'unité de mesure et mettre format sur même unité (ici: gramme)
         format_produit = resultat.find('td', {'width': '86'}).text.strip()
-        if format_produit[-2:] == 'kg':
-            format_produit_grammes = int(format_produit[:-2]) * 1000
-        else:
-            format_produit_grammes = int(format_produit[:-2])
-
-        # calculer coût par 100 g
-        cout_100g_produit = prix_produit / (format_produit_grammes / 100)
 
         # créer dictionnaire des résultats
-        details_produit = {'nom': nom_produit[:40], 'format': format_produit, 'origine': origine_produit, 'prix': str('{:.2f}'.format(prix_produit)) + ' $', 'prix / 100 g': str('{:.2f}'.format(cout_100g_produit)) + ' $', 'période spécial': periode_special_produit, 'magasin': magasin_produit}
+        details_produit = {'nom': nom_produit[:40], 'format': format_produit, 'origine': origine_produit, 'prix': str('{}'.format(prix_produit)) + ' $', 'période spécial': periode_special_produit, 'magasin': magasin_produit}
 
         # ajouter le dictionnaire à la liste de résultats
         liste_speciaux.append(details_produit)
 
     return(liste_speciaux)
 
-def extraire_json(liste_speciaux, fichier_sortie):
+def sortie_json(liste_speciaux, fichier_sortie):
 
-    with open(fichier_sortie, 'w', encoding='utf-8') as outfile:
+    with open(fichier_sortie, 'a', encoding='utf-8') as outfile:
         json.dump(liste_speciaux, outfile, indent = 4, sort_keys = True)
 
 def main():
@@ -93,7 +83,7 @@ def main():
     # variables
     recherche_url_base = 'http://www.supermarches.ca/pages/default.asp?t=&tr=&vd=&ig=&q=rech&cid=&query='
     ajd = datetime.now().strftime("%Y%m%d")
-    fichier_sortie = 'liste_speciaux_' + ajd + '.json'
+
     fichier_mots = 'mots_recherche.yml'
 
     # extraire les mots clé à chercher
@@ -111,8 +101,9 @@ def main():
             soupe_resultats = trouver_resultats(soupe)
 
             # faire liste des spéciaux et sortir en json
-            liste_speciaux = creer_liste(soupe_resultats)
-            extraire_json(liste_speciaux, fichier_sortie)
+            fichier_sortie = 'liste_speciaux_' + mot[:6] + ajd + '.json'
+            liste_speciaux = creer_liste(soupe_resultats, mot)
+            sortie_json(liste_speciaux, fichier_sortie)
 
         else:
             print('Pas de spéciaux pour {}'.format(mot).replace('+', ' '))
