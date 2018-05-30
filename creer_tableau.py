337169b8e06eba51363edb568ebca65a8c65cbe4
Normand Cyr <normand.cyr@gmail.com>
2018-05-29 23:56:35 -0400

    préparer pour utiliser une liste d'items à chercher

--- creer_tableau.py
+++ creer_tableau.py
@@ -6,28 +6,21 @@ from datetime import datetime
 from json2html import *
 from tabulate import tabulate
 
-def faire_tableau_html(fichier_json, fichier_html):
+def faire_tableau_html(texte_json, fichier_html):
 
-    with open(fichier_json, 'r', encoding='utf-8') as f:
-        texte_json = json.load(f)
-        tableau_html = json2html.convert(json = texte_json)
-
+    tableau_html = json2html.convert(json = texte_json)
     with open(fichier_html, 'w') as f2:
         f2.write(tableau_html)
 
     return(tableau_html)
 
-def faire_tableau_texte(fichier_json, fichier_texte, ajd):
-
-    # lire les données JSON
-    with open(fichier_json, 'r', encoding='utf-8') as f:
-        texte_json = json.load(f)
+def faire_tableau_texte(texte_json, fichier_texte, ajd):
 
     # créer le tableau texte avec le module tabulate
     tableau_texte = tabulate(texte_json, headers = 'keys')
 
     # enregistrer en fichier TXT
-    with open(fichier_texte, 'w', encoding='utf-8') as f:
+    with open(fichier_texte, 'w') as f:
         f.write('Spéciaux sur le beurre d\'arachide.\n')
         f.write('Généré le {}.\n'.format(ajd))
         f.write('\n')
@@ -39,12 +32,19 @@ def main():
 
     ajd = datetime.now().strftime("%Y%m%d")
 
-    fichier_json = 'liste_speciaux_' + ajd + '.json'
+    produit = 'Beurre'
+
+    fichier_json = 'liste_speciaux_' + produit + ajd + '.json'
     fichier_html = 'liste_speciaux_' + ajd + '.html'
     fichier_texte = 'liste_speciaux_' + ajd + '.txt'
 
-    #tableau_html = faire_tableau_html(fichier_json, fichier_html)
-    tableau_texte = faire_tableau_texte(fichier_json, fichier_texte, ajd)
+    # lire les données JSON
+    with open(fichier_json, 'r') as f:
+        texte_json = json.load(f)
+        tableau_texte = faire_tableau_texte(texte_json, fichier_texte, ajd)
+        #print(tableau_texte)
+
+    #tableau_html = faire_tableau_html(texte_json, fichier_html)
 
 if __name__ == '__main__':
     main()
